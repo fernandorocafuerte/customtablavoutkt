7da969c5f7c171d24d3268f14f2f1363d4614853
bryan <rocafueteherrera@gmail.com>
2023-05-08 22:33:02 -0500

    Hicimos dos fragmentos con customtablayout

---
+++ app/src/main/java/com/example/customtablavoutkt/FragmentPageAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.customtablavoutkt
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FragmentPageAdapter (
+    fragmetManager: FragmentManager,
+    lifecycle: Lifecycle
+    ) : FragmentStateAdapter(fragmetManager, lifecycle){
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return if (position == 0)
+            FirstFragment()
+        else
+            SecondFragment()
+
+    }
+
+
+}
